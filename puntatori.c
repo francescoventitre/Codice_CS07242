973d5dd2ea75b2ad6e3f0e81f42ae71eff1afad8
francescoventitre <francesco.orru23@gmail.com>
2024-11-12 13:24:37 +0100

    UP

---
+++ puntatori.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void scriviVettore(int *ptr)
+{
+    for (int i=0; i < 10; i++){
+        printf("\nInserisci n[%d]: ", i);
+        scanf("%d", ptr);
+        ptr++;
+    }
+}
+
+void leggiVettore(int *ptr)
+{
+    for (int i=0; i<10; i++){
+        printf("\nElemento n[%d] = %d (zona memoria 0x%x)", i, *ptr, ptr);
+    ptr++;
+    }
+}
+
+int main (){
+    int n[10] = {0};
+    int *n_ptr;
+
+    n_ptr = &n[0];
+
+    scriviVettore (n_ptr);
+    leggiVettore (n_ptr);
+}
