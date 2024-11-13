f65b335ed39285f1d6c20c6155f05a74b05e91de
francescoventitre <francesco.orru23@gmail.com>
2024-11-13 10:32:46 +0100

    UP

---
+++ Python/matematicaBase.py
@@ -0,0 +1,9 @@
+a=30
+b=70
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+print(f"{a}*{b}={a*b}")
+print(f"{a}/{b}={a/b}")
+print(f"{a}//{b}={a//b}") #divisione intera
+print(f"{a}%{b}={a%b}") #resto o modulo
+print(f"{a}**3={a**3}") #potenza
